7ec3de95b4d1aa37e10b63022b724438b16b3b4b
hwanyhee <hwanyhee@naver.com>
2019-08-10 12:05:43 +0900

    190810 12:05

--- webscrawling/__init__.py
+++ webscrawling/__init__.py
@@ -1,11 +1,14 @@
 from webscrawling.controller import Controller
 if __name__ == '__main__':
-    print('a.국회 크롤링')
-    print('b.벅스 크롤링')
-    print('f.네이버 크ㄹ로링')
+    while True:
+        print('a.국회 크롤링')
+        print('b.벅스 크롤링')
+        print('f.네이버 크ㄹ로링')
 
-    print('0.종료')
-    menu = input('메뉴선택?\n')
-    ctrl = Controller()
-    ctrl.exec(menu)
+        print('0.종료')
+        menu = input('메뉴선택?\n')
+        if menu == '0':
+            break
+        ctrl = Controller()
+        ctrl.exec(menu)
 
