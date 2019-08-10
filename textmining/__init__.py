48bc405982ee7577a685d03199d35d0fe803a6e3
hwanyhee <hwanyhee@naver.com>
2019-08-10 16:55:59 +0900

    190810 16:55

--- textmining/__init__.py
+++ textmining/__init__.py
@@ -1,5 +1,5 @@
 from textmining.model import SamsungReport
 if __name__ == '__main__':
     sr = SamsungReport()
-    sr.download()
-    #print(sr.extract_noun())
+    #sr.download()
+    print(sr.extract_noun())
